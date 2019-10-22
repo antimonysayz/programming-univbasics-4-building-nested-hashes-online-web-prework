9a26cb3afb2f9ff7c970fbc010d719e04dcc74d7
Anthony Mai <anthony.mai530@gmail.com>
2019-10-22 00:06:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:Reading => {}, :Pennsylvania => {}, :b_and_o_railroad => {}, :shortline => {}}, :names => {:Reading_Railroad = {}, :Pennsylvania_Railroad =P{}}
+  hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:Reading => {}, :Pennsylvania => {}, :b_and_o_railroad => {}, :shortline => {}}, :names => {:Reading_Railroad = {}, :Pennsylvania_Railroad => {},}
     
     
   }
